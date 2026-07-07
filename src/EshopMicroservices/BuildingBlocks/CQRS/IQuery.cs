76f776d63cefca8fbc8ff73d83e8f8457b77a94e
Gowtham Reddy <gowthammanikanta07@gmail.com>
2026-07-07 09:17:38 +0100

    CQRS with Validation, Exception Handling and Logging (Common Libraries across all microservices)

---
+++ src/EshopMicroservices/BuildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQuery : IRequest<Unit>
+    {
+
+    }
+    public interface IQuery<out TResponse> : IRequest<TResponse> 
+    {
+    }
+}
