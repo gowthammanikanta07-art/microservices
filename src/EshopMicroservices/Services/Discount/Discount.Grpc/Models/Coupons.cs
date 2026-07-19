defc15f096a10b41f57877467d2627d447acc099
Gowtham Reddy <gowthammanikanta07@gmail.com>
2026-07-20 00:55:12 +0100

    Discount Service base setup

---
+++ src/EshopMicroservices/Services/Discount/Discount.Grpc/Models/Coupons.cs
@@ -0,0 +1,10 @@
+﻿namespace Discount.gRPC.Models
+{
+    public class Coupons
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public decimal Amount { get; set; } 
+    }
+}
